ca3be8e105eaf299e4a176df75a9e0516ccbc4f6
ishowvel <rakshit.c16@gmail.com>
2024-12-11 20:41:19 +0530

    chore: remove unused listeners

--- src/types/context.ts
+++ src/types/context.ts
@@ -4,7 +4,7 @@ import { Env } from "./env";
 import { createAdapters } from "../adapters";
 import { Command } from "./command";
 
-export type SupportedEvents = "issue_comment.created" | "pull_request.opened" | "pull_request.ready_for_review";
+export type SupportedEvents = "pull_request.opened" | "pull_request.ready_for_review";
 
 export type Context<TEvents extends SupportedEvents = SupportedEvents> = PluginContext<PluginSettings, Env, Command, TEvents> & {
   adapters: ReturnType<typeof createAdapters>;
