358a6210ecb90c4977a4f918bdb9c07ea5d0356d
ishowvel <rakshit.c16@gmail.com>
2024-12-13 18:45:19 +0530

    chore: remove unused stuff

--- src/types/github-types.ts
+++ src/types/github-types.ts
@@ -1,11 +1,3 @@
 import { RestEndpointMethodTypes } from "@octokit/rest";
-import { Context } from "./context";
 
 export type Issue = RestEndpointMethodTypes["issues"]["get"]["response"]["data"];
-
-export type FetchParams = {
-  context: Context;
-  issueNum?: number;
-  owner?: string;
-  repo?: string;
-};
